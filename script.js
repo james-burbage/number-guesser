bee370693c7920f2d6f742cef0d59b48155c4f27
James Burbage <jeburbage@gmail.com>
2018-05-10 09:08:20 -0600

    Finish

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Variables
 var clearButton = document.querySelector('.clear-button');
 var displayGuess = document.querySelector('.last-guess');
 var explainText = document.querySelector('.explain-text');
@@ -7,15 +6,17 @@ var inputBox = document.querySelector('.input-box');
 var outputText = document.querySelector('.output-text');
 var randNum = Math.floor(Math.random() * 100 +1);
 var resetButton = document.querySelector('.reset-button');
+var maxRangeNum = document.querySelector('.range-max-box').value || 100;
+var minRangeNum = document.querySelector('.range-min-box').value || 1;
+var rangeSubmit = document.querySelector('.range-submit');
 
-// Event Listeners
 guessButton.addEventListener('click', checkGuess);
 inputBox.addEventListener('keyup', buttonsEnable);
 resetButton.addEventListener('click', resetGame);
+rangeSubmit.addEventListener('click', getRandomNumber);
 
-// Functions
 function buttonsEnable() {
-  // clearButton.disabled = false;
+  clearButton.disabled = false;
   guessButton.disabled = false;
   resetButton.disabled = false;
   console.log("buttons work!");
@@ -23,32 +24,33 @@ function buttonsEnable() {
 
 function checkGuess(event) {
   event.preventDefault();
+  getRandomNumber();
   var parsedNum = parseInt(inputBox.value);
+  var parsedMax = parseInt(maxRangeNum);
+  var parsedMin = parseInt(minRangeNum);
   displayGuess.innerText = inputBox.value;
   explainText.innerText = 'Your last guess was:';
-    if (parsedNum === randNum) {
+    if (parsedNum > parsedMax || parsedNum < parsedMin) {
+  outputText.innerText = "Invalid number! Please pick a number between 1 - 100!";
+} else if (parsedNum === randNum) {
   outputText.innerText = "BOOM!!!";
 } else if (parsedNum < randNum) {
   outputText.innerText = "That is too low!";
 } else if (parsedNum > randNum) {
   outputText.innerText = "That is too high!";
-} else if (parsedNum > 100) {
-  outputText.innerText = "Invalid number. Please pick a number between 1-100";
-} else if (parsedNum < 1) {
-  outputText.innerText = "Invalid number. Please pick a number between 1-100";
 }
   console.log(randNum);
   console.log('guess checked!')
 }
 
 function resetGame() {
-  // inputBox.value = '';
-  // displayGuess.innerText = inputBox.value;
-  // outputText.innerText = inputBox.value;
   location.reload();
   console.log('input reset!');
 }
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * 100 +1);
+  var parsedMax = parseInt(maxRangeNum);
+  var parsedMin = parseInt(minRangeNum);
+  return Math.floor(Math.random() * parsedMax + 1 - parsedMin) + parsedMin;
+  console.log('number generated!')
 }
